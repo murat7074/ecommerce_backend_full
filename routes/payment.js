2eaf17f76f8ce9b89f543aff84f9afd96cd4c9f8
murat7074 <muratyonev@gmail.com>
2024-06-27 17:17:20 +0300

    first changing

--- routes/payment.js
+++ routes/payment.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 const router = express.Router();
 
@@ -11,5 +12,33 @@ router
   .route("/payment/checkout_session")
   .post(isAuthenticatedUser, stripeCheckoutSession);
 
-router.route("/payment/webhook").post(stripeWebhook);
+// Webhook için raw body kullanarak middleware
+router.route("/payment/webhook").post(express.raw({ type: 'application/json' }), stripeWebhook);
+
 export default router;
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const router = express.Router();
+
+// import { isAuthenticatedUser } from "../middlewares/auth.js";
+// import {
+//   stripeCheckoutSession,
+//   stripeWebhook,
+// } from "../controllers/paymentControllers.js";
+
+// router
+//   .route("/payment/checkout_session")
+//   .post(isAuthenticatedUser, stripeCheckoutSession);
+
+// router.route("/payment/webhook").post(stripeWebhook);
+// export default router;
