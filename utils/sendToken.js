ff0e747d704afe72e0b73b4c9eb27fd330df29c1
murat7074 <muratyonev@gmail.com>
2024-06-29 21:07:23 +0300

    Commit mesajı

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,17 +1,19 @@
 // Create token and save in the cookie
 export default (user, statusCode, res) => {
   // Create JWT Token
-  const token = user.getJwtToken();
+  const token = user.getJwtToken()
 
   // Options for cookie
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true, 
-  };
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'PRODUCTION', // production'da secure flag'ini ekleyin
+   sameSite: 'None', // üçüncü taraf cookie'leri için None, CSRF saldırılarına karşı koruma
+  }
 
-  res.status(statusCode).cookie("token", token, options).json({
+  res.status(statusCode).cookie('token', token, options).json({
     token,
-  });
-};
+  })
+}
